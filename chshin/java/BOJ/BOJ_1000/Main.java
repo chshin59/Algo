21e09d887783eaf7b79a5fae3a637f359e7b8864
chshin59 <chshin59@gmail.com>
2023-02-03 00:16:51 +0900

    BOJ 새싹문제(입력과 계산) java

---
+++ chshin/java/BOJ/BOJ_1000/Main.java
@@ -0,0 +1,19 @@
+// BOJ_1000 A+B
+package BOJ.BOJ_1000;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // String str = br.readLine();
+        // StringTokenizer st = new StringTokenizer(str, " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        System.out.println(A + B);
+    }
+}
---
+++ chshin/java/BOJ/BOJ_11382/Main.java
@@ -0,0 +1,19 @@
+// BOJ_11382 꼬마 정민
+package BOJ.BOJ_11382;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        long result = 0;
+        for (int i = 0; i < 3; i++) {
+            result += Long.parseLong(st.nextToken());
+        }
+        System.out.println(result);
+    }
+}
